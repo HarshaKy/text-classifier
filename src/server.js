a6992c28a5e1c6169b78ad0bbca046061b9af99d
HarshaKy <harshaky6573@gmail.com>
2020-03-30 14:37:28 +0530

    fixes

--- src/server.js
+++ src/server.js
@@ -70,31 +70,37 @@ app.post('/predict-category', (req, res) => {
 
     async function processModel(){
         const model = await tf.loadLayersModel('file://../models/news/model.json')
-        
-        console.log('before python')
 
         var spawn = require("child_process").spawnSync
-        var process = await spawn('python',["./../utils/preprocess-news.py", req.body.test] );
-
-        console.log(JSON.parse(process.stdout))
+        var process = await spawn('python',["./../utils/preprocess-news.py", req.body.test]);
 
         var data = JSON.parse(process.stdout)
 
         prediciton = model.predict(tf.tensor(data)).dataSync()
 
-        var pred
-
-        var predString = ""
-
-        for (pred in prediciton) {
-            predString += prediciton[pred].toString() + " "
+        var arr = {
+            "Black Voices": prediciton[0], 
+            "Business ": prediciton[1], 
+            "Comedy ": prediciton[2], 
+            "Entertainment ": prediciton[3], 
+            "Food and Drink": prediciton[4], 
+            "Healthy Living": prediciton[5], 
+            "Home Living": prediciton[6], 
+            "Parenting ": prediciton[7], 
+            "Parents ": prediciton[8], 
+            "Politics ": prediciton[9], 
+            "Queer Voices": prediciton[10], 
+            "Sports ": prediciton[11], 
+            "Style and Beauty": prediciton[12], 
+            "Travel ": prediciton[13], 
+            "Wellness ": prediciton[14]
         }
 
-        console.log(predString)
+        console.log(arr)
 
         res.render('prediction', {
             title: 'Category Prediction',
-            prediction: predString
+            prediction: JSON.stringify(arr)
         })
 
     }
